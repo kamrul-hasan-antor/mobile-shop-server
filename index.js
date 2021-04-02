0feeb9aa4413df96c9c44c9c350dbc1c349faabd
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2021-04-03 03:48:05 +0600

    server not done

--- index.js
+++ index.js
@@ -13,45 +13,47 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dcrxy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o8ccw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 client.connect((err) => {
-  const productsCollection = client
+  const productCollection = client
     .db(`${process.env.DB_NAME}`)
     .collection(`${process.env.DB_COLLECTION}`);
   const productCollectionForOrder = client
     .db(`${process.env.DB_NAME}`)
     .collection(`${process.env.DB_COLLECTION}`);
 
-  app.get("/products", (req, res) => {
-    productsCollection.find().toArray((err, items) => {
+  app.get("/events", (req, res) => {
+    productCollection.find().toArray((err, items) => {
       res.send(items);
     });
   });
 
-  app.post("/addProduct", (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct);
-    productsCollection.insertOne(newProduct).then((result) => {
+  app.post("/addEvent", (req, res) => {
+    const newEvent = req.body;
+    console.log("adding new event: ", newEvent);
+    productCollection.insertOne(newEvent).then((result) => {
+      console.log("inserted count", result.insertedCount);
       res.send(result.insertedCount > 0);
     });
   });
 
-  //   client.close();////////////
   app.get("/checkout/:_id", (req, res) => {
-    productsCollection
+    console.log(req.params._id);
+    productCollection
       .find({ _id: ObjectId(req.params._id) })
+
       .toArray((err, documents) => {
         res.send(documents[0]);
       });
   });
 
   app.post("/addOrders", (req, res) => {
-    const newOrders = req.body;
-    productCollectionForOrder.insertOne(newOrders).then((result) => {
+    const newOrder = req.body;
+    productCollectionForOrder.insertOne(newOrder).then((result) => {
       res.send(result.insertedCount > 0);
     });
   });
@@ -61,8 +63,9 @@ client.connect((err) => {
       res.send(items);
     });
   });
+
   app.delete("/delete/:_id", (req, res) => {
-    productsCollection
+    productCollection
       .deleteOne({ _id: ObjectId(req.params.id) })
       .then((result) => {
         res.send(result.deletedCount > 0);
@@ -71,7 +74,7 @@ client.connect((err) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("hello world!!!");
+  res.send("Hello Bangladesh");
 });
 
 app.listen(port);
