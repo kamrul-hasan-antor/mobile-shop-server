0087b171844a72f21c985fb8c7e8c6c72c95ef56
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2021-04-02 18:15:10 +0600

    server trying

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const app = express();
 const MongoClient = require("mongodb").MongoClient;
-const cors = require("cors");
 const bodyParser = require("body-parser");
-require("dotenv").config();
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("hello world!!!!!!!!!!!");
-});
+require("dotenv").config();
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dcrxy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -43,7 +44,7 @@ client.connect((err) => {
   //   client.close();////////////
   app.get("/checkout/:_id", (req, res) => {
     console.log(req.params._id);
-    productCollection
+    productsCollection
       .find({ _id: ObjectId(req.params._id) })
 
       .toArray((err, documents) => {
@@ -64,7 +65,7 @@ client.connect((err) => {
     });
   });
   app.delete("/delete/:_id", (req, res) => {
-    productCollection
+    productsCollection
       .deleteOne({ _id: ObjectId(req.params.id) })
       .then((result) => {
         res.send(result.deletedCount > 0);
@@ -72,6 +73,8 @@ client.connect((err) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.send("hello world!!!!!!!!!!!");
 });
+
+app.listen(port);
